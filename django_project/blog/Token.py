7c409b6ecbe48913e5fcb4ffeb3da5f6bc4e6579
bianQ <vagaab@foxmail.com>
2016-11-04 22:05:42 +0800

    add email auth

---
+++ django_project/blog/Token.py
@@ -0,0 +1,19 @@
+from itsdangerous import URLSafeTimedSerializer as uts
+import base64
+from django.conf import settings as django_settings
+
+class Token:
+    def __init__(self, security_key):
+        self.serializer = uts(security_key)
+        self.salt = base64.encodebytes(security_key.encode())
+
+    def generate_validate_token(self, username):
+        return self.serializer.dumps(username, self.salt)
+
+    def confirm_validate_token(self, token, expiration=3600):
+        return self.serializer.loads(token, salt=self.salt)
+
+    def remove_validate_token(self, token):
+        return self.serializer.loads(token, salt=self.salt)
+
+token_confirm = Token(django_settings.SECRET_KEY)
